1ccdecce71cb54774e91f66ce60d6a0ea69b49c1
AdityaNath1221 <adityanath1221@gmail.com>
2026-01-06 14:14:12 +0530

    slight change in shell script

--- vibetui.sh
+++ vibetui.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash 
 set -e
 
-if ! command -v python >/dev/null 2>&1; then
+if ! command -v python3 >/dev/null 2>&1; then
     echo "Python not found. Install latest version of python first!!"
     exit 1
 fi
 
 if [ ! -d ".env" ]; then
     echo "Creating Virtual Environment..."
-    python -m venv .env
+    python3 -m venv .env
     source .env/bin/activate
     pip install -r requirements.txt
 fi
@@ -17,6 +17,6 @@ source .env/bin/activate
 
 echo "Launching..."
 cat assets/logo.txt
-python VIBEtui.py
+python3 VIBEtui.py
 
 
